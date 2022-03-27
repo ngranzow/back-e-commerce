47db44ecbc22d4c03840f6398b2e1bdb0f878335
ngranzow <nate.granzow@gmail.com>
2022-03-27 12:28:43 -0500

    fixed product-routes delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -58,7 +58,7 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  Product.create(req.body)({
+  Product.create({
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
